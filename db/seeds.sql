2bec450fa0646cbb631078080c002728b5ffdc42
Dennis The Menace <sdennisp25@hotmail.com>
2018-11-03 18:16:59 -0600

    created connection/burgers.js/controller

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegan Burger', true);
