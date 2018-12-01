bf89361e95969e989703b15013e1abc38189ba1a
Dennis The Menace <sdennisp25@hotmail.com>
2018-12-01 11:31:48 -0700

    working on JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,9 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -9,7 +12,9 @@ var connection = mysql.createConnection({
   database: "burgers_db"
 });
 
+
 // Make connection.
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
